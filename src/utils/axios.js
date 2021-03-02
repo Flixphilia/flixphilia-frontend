7f6ac4cdddd4b0b0c861fb0221e902c8833be84c
apurva1706 <apurva.peri2001@gmail.com>
2021-03-02 15:00:34 +0530

    🔧 sorted

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-const proxy = 'https://cors-anywhere-flixphilia.herokuapp.com/';
-
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: `${proxy}https://pure-lake-91665.herokuapp.com/api/`,
+  baseURL: `https://pure-lake-91665.herokuapp.com/api/`,
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
